c7d980b1be30db839894caddd45e80b23ae264b7
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2023-11-21 20:50:36 +0530

    desktop grid created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,25 @@
 // import FirstLogin from "../../components/first-login/FirstLogin";
 import { BsQrCodeScan, BsQrCode } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 const Home = () => {
+    /*
+     *
+     * @param https://bobbyhadz.com/blog/javascript-create-array-n-elements-same-value
+     *
+     *
+     */
+    const [streak, setStreak] = useState(Array(30).fill(true));
+
+    useEffect(() => {
+        console.log(streak);
+    }, []);
+
     const navigate = useNavigate();
     return (
         <div className="w-full mt-12  min-h-max flex flex-col justify-center items-center ">
             {/* <FirstLogin /> */}
-            <div className="bg-white p-4 gap-3 w-3/4 md:w-2/5 flex flex-col justify-center items-center rounded-md shadom-2xl">
+            <div className="bg-white p-4 gap-12 w-3/4 md:w-2/5 flex flex-col justify-center items-center rounded-md shadom-2xl">
                 <h2 className="text-center text-xl ">task name</h2>
 
                 <div className="flex w-full items-center  justify-between p-2">
@@ -28,7 +41,22 @@ const Home = () => {
                     </button>
                 </div>
 
-                <div></div>
+                <div className="grid grid-rows-4 grid-flow-col gap-4">
+                    {streak.map((value, index) => {
+                        return (
+                            <div
+                                key={index}
+                                className={`${
+                                    value ? "bg-green-600" : "bg-gray-600"
+                                } shadow-xl px-2 py-1 w-full h-full`}
+                            >
+                                <span className="font-bold text-center text-white">
+                                    {index}
+                                </span>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
