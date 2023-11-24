b69e9d5eef1ebbe770532acec56a717d336da186
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2023-11-24 23:30:29 +0530

    // !todo - Write logic to download Qr

--- src/components/show-qr/ShowQR.jsx
+++ src/components/show-qr/ShowQR.jsx
@@ -1,15 +1,34 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import QRCode from "react-qr-code";
+const options = {
+    allowTaint: true,
+    useCORS: true,
+    backgroundColor: "rgba(0,0,0,0)",
+    removeContainer: true,
+};
 
 const ShowQR = () => {
     const [showQrCode, setShowQrCode] = useState(false);
     const [name, setName] = useState("");
+
+    const qrRef = useRef();
     const generateQRCode = () => {
         const name = localStorage.getItem("name");
         if (name) {
             setName(name);
         } else {
             alert("Please enter Name");
+        }
+    };
+
+    const downloadMyQr = async () => {
+        const cardElement = qrRef.current;
+
+        if (!cardElement) return;
+        try {
+            // !todo - Write logic to download Qr
+        } catch (reason) {
+            console.log(reason);
         }
     };
 
@@ -28,14 +47,23 @@ const ShowQR = () => {
             <div>
                 {showQrCode && (
                     <QRCode
+                        ref={qrRef}
                         name="ScanToAddAttendance"
                         size={200}
                         value={name}
+                        id="myQr"
                     />
                 )}
             </div>
             <h1 className="pt-5">Please Save this QR Code</h1>
-            <button className="px-2 py-1 border rounded-md ">Download</button>
+            <button
+                onClick={() => {
+                    downloadMyQr();
+                }}
+                className="px-2 py-1 border rounded-md "
+            >
+                Download
+            </button>
         </div>
     );
 };
