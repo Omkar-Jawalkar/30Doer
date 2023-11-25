ddf4c11864934137a8c163983567838ad43fa7fb
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2023-11-25 19:31:00 +0530

    PWA demo template done

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "./i18n/config.js";
+import { registerSW } from "virtual:pwa-register";
+
+// add this to prompt for a refresh
+const updateSW = registerSW({
+    onNeedRefresh() {
+        if (confirm("New content available. Reload?")) {
+            updateSW(true);
+        }
+    },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        VitePWA({
+            // add this to cache all the imports
+            workbox: {
+                globPatterns: ["**/*"],
+            },
+            // add this to cache all the
+            // static assets in the public folder
+            includeAssets: ["**/*"],
+            manifest: {
+                theme_color: "#16a221",
+                background_color: "#ffffff",
+                display: "browser",
+                scope: "/",
+                start_url: "/",
+                name: "30 Doer",
+                short_name: "30Doer",
+                icons: [
+                    {
+                        src: "/icon-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icon-256x256.png",
+                        sizes: "256x256",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icon-384x384.png",
+                        sizes: "384x384",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icon-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
+    ],
+});
