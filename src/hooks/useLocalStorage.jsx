cf506beee4586787e875657842c2a4b07326c298
Omkar-Jawalkar <omkarjawalkar234@email.com>
2023-11-30 19:16:47 +0530

    In implementation of useLocalStorage

--- src/hooks/useLocalStorage.jsx
+++ src/hooks/useLocalStorage.jsx
@@ -4,4 +4,29 @@
  *https://www.geeksforgeeks.org/reactjs-uselocalstorage-custom-hook/
  *
  */
-// export const useLocalStorage = {};
+
+// import { useState, useEffect } from "react";
+
+// const useLocalStorage = ({ key, valueOrFunction }) => {
+//     const [state, setState] = useState(() => {
+//         try {
+//             let value = localStorage.getItem(key);
+//             if (value) {
+//                 return JSON.parse(value);
+//             } else {
+//                 return valueOrFunction;
+//             }
+//         } catch {
+//             return valueOrFunction;
+//         }
+//     });
+
+//     const updateState = (valueOrFunction) => {
+        
+
+//     };
+
+//     return <div>useLocalStorage</div>;
+// };
+
+// export default useLocalStorage;
