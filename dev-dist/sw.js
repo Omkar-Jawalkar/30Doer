71987620bddd66820b8fc7eebe355c4e30df41bf
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2023-12-06 17:29:56 +0530

    white background added

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.1ot3vjamueo"
+    "revision": "0.2hs5teisis"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/show-qr/ShowQR.jsx
+++ src/components/show-qr/ShowQR.jsx
@@ -16,11 +16,21 @@ const ShowQR = () => {
         const cardElement = qrRef.current;
         if (!cardElement) return;
         try {
-            html2canvas(qrRef.current).then(function (canvas) {
+            html2canvas(qrRef.current).then(function (originalCanvas) {
+                const padding = 20;
+                const paddedCanvas = document.createElement("canvas");
+                const context = paddedCanvas.getContext("2d");
+                paddedCanvas.width = originalCanvas.width + 2 * padding;
+                paddedCanvas.height = originalCanvas.height + 2 * padding;
+                context.fillStyle = "white";
+                context.fillRect(0, 0, paddedCanvas.width, paddedCanvas.height);
+                context.drawImage(originalCanvas, padding, padding);
+
                 const link = document.createElement("a");
-                link.download = `${name}qr.jpg`;
+                link.download = `${name?.value}qr.jpg`;
                 link.style.padding = "1000px";
-                link.href = canvas.toDataURL("image/jpg");
+                link.style.backgroundColor = "white";
+                link.href = paddedCanvas.toDataURL("image/jpg");
                 link.click();
             });
         } catch (reason) {
