36c800477d0a3f8fddfcc644c9f202563583afbb
Omkar-Jawalkar <omkar@goreeva.com>
2023-12-09 23:26:19 +0530

    Update sw.js and Aboutus.jsx

--- src/pages/aboutus/Aboutus.jsx
+++ src/pages/aboutus/Aboutus.jsx
@@ -1,9 +1,15 @@
 const Aboutus = () => {
     return (
-        <div className="flex gap-1 my-6 justify-center items-center">
+        <div className="flex gap-1 my-6 flex-col justify-center items-center">
             <h1 className="text-2xl font-bold">About us</h1>
 
             {/* ABOUT US MORE CONTENT */}
+            <p className="text-center">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Minima, incidunt commodi quasi vitae quibusdam, nesciunt eius
+                repellat explicabo at et quaerat! Fugit, corrupti? Repudiandae
+                fuga fugiat, modi nobis aliquid corrupti.
+            </p>
         </div>
     );
 };
