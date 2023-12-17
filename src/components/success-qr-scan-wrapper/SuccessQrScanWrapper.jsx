27eab00d18d62c0b9ca153cdb55538084146d9cb
Omkar-Jawalkar <omkar@goreeva.com>
2023-12-17 17:58:19 +0530

    Animation added

---
+++ src/components/success-qr-scan-wrapper/SuccessQrScanWrapper.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import ShowScanSuccessPage from "../../pages/show-scan-success-page/ShowScanSuccessPage";
+import Html5QrcodePlugin from "../../plugins/Html5QrcodePlugin";
+
+const SuccessQrScanWrapper = () => {
+    const [showScanSuccess, setShowScanSuccess] = useState(true);
+
+    if (showScanSuccess) {
+        return <ShowScanSuccessPage />;
+    }
+    return (
+        <Html5QrcodePlugin
+            showScanSuccess={showScanSuccess}
+            setShowScanSuccess={setShowScanSuccess}
+        />
+    );
+};
+
+export default SuccessQrScanWrapper;
---
+++ src/pages/show-scan-success-page/ShowScanSuccessPage.jsx
@@ -0,0 +1,10 @@
+const ShowScanSuccessPage = () => {
+    return (
+        <div className="flex gap-4 flex-col justify-center items-center">
+            <h1>Success animation</h1>
+            <button className="px-2 py-1 border rounded-md">Okay</button>
+        </div>
+    );
+};
+
+export default ShowScanSuccessPage;
