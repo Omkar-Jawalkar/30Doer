0b74ed09f731436b4f3fed2496ed8b9973427890
Omkar-Jawalkar <omkar@goreeva.com>
2023-12-19 18:48:30 +0530

    Update sw.js and ShowScanSuccessPage.jsx, and remove Html5QrcodePlugin import

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.rkpnu50pt28"
+    "revision": "0.juch04neltg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/pages/show-scan-success-page/ShowScanSuccessPage.jsx
+++ src/pages/show-scan-success-page/ShowScanSuccessPage.jsx
@@ -1,8 +1,29 @@
+import { IoIosCheckmarkCircle } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+
 const ShowScanSuccessPage = () => {
+    const navigate = useNavigate();
     return (
-        <div className="flex gap-4 flex-col justify-center items-center">
-            <h1>Success animation</h1>
-            <button className="px-2 py-1 border rounded-md">Okay</button>
+        <div className="flex  gap-4 flex-col justify-center items-center">
+            <div className="my-4 flex flex-col justify-center items-center">
+                {/* ANIMATION */}
+                <IoIosCheckmarkCircle
+                    style={{
+                        animation: "3s  lineral infinite",
+                    }}
+                    className="text-[#2C5F2D] text-4xl"
+                />
+                <h1 className="text-[#2C5F2D]">Attendance Success</h1>
+                <p className="text-slate-400 text-xs">Lets Go</p>
+            </div>
+            <button
+                onClick={() => {
+                    navigate("/");
+                }}
+                className="px-2 py-1 border rounded-md"
+            >
+                Lets go
+            </button>
         </div>
     );
 };
